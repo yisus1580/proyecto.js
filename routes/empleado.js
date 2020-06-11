ed5074433e2bacab280e6b76c7a5a5c305ba83ba
yisus1580 <tempor1580@outlook.com>
2020-06-11 00:02:13 -0500

    Interfaz

--- routes/empleado.js
+++ routes/empleado.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const empleado = express.Router(); 
-const db = require('../config/empleado.sql');
+const db = require('../config/database');
 
 empleado.post("/",async(req,res,next)=>{
 	const {emp_name , emp_lastname , emp_mail, emp_phone , emp_address} = req.body;
